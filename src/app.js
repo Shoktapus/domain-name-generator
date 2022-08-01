fc9e738254739e1e0fd54e9262e1b0692b3545dd
Shoktapus <sinclair8075@gmail.com>
2022-08-01 23:51:29 +0000

    finished for now

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let value = "3wineverlisten.com";
+  const pronouns = ["my", "our", "your", "their"];
+  const adj = ["superficial", "odd", "silly", "swift"];
+  const nouns = ["efficiency", "climate", "guitar", "initiative"];
+  const domains = [".com", ".org", ".io", ".us"];
+
+  const getRandomPro = pronouns[Math.floor(Math.random() * pronouns.length)];
+  const getRandomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const getRandomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  const getRandomDomain = domains[Math.floor(Math.random() * domains.length)];
+  value = getRandomPro + getRandomAdj + getRandomNoun + getRandomDomain;
+
+  document.getElementById("martin").innerHTML = value;
 };
